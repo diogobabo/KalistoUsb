a835f5cb6c20b072fec2d30dccbb946628cfee2b
RicardoCavalheiro123 <up202005103@fe.up.pt>
2023-03-08 17:32:48 +0000

    3.1 and 3.2 need guidance; 3.3 finished; 3.10, 3.12 done

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import serial.tools.list_ports
 import serial
 
 
+
+
+
+# Auxiliary Functions
+
+# Convert a string to hex
 def string_to_hex(hexa):
     hexa = '0x' + hexa
     an_integer = int(hexa, 16)
@@ -49,16 +55,32 @@ class Kallisto:
             else:
                 break
         return result
+    
 
-    # 3.5 (sensor_name, true/false (enable,disable), time)
-    def set_sensor(self, sensor, status, interval):
+    # 3.1 Set Storage
+    # Enables and disables data storage in Kallisto. Each sensor is enabled separately. Sensor ID and Filename must be provided.
+    def set_storage(self, sensor, status, length, filename):
         if status:
             status = 0x01
         else:
             status = 0x00
-        if self.dict[sensor] == 0x01 or self.dict[sensor] == 0x02 or self.dict[sensor] == 0x03:
-            interval = interval * 1000
-        self.write([0x05, self.dict[sensor], status] + list(interval.to_bytes(4, 'big')))
+        self.write([0x01, self.dict[sensor], status, length, 0x74, 0x2e])
+        res = self.read()
+        print(res)
+        if res != ['02', '00', '0a']:
+            print("Error")
+            return False
+        return True
+
+    # 3.2 Get Storage 
+    # Requests Kallisto to send data in the file with the provided Filename
+    def get_storage(self, length, filename):
+        self.write([0x03, length, filename])
+        res = self.read()
+        print(res)
+        return True
+
+
 
     # 3.3 (sensor_name, true/false (enable,disable))
     def set_stream(self, sensor, status):
@@ -75,6 +97,53 @@ class Kallisto:
             return False
         return True
 
+
+    # 3.5 (sensor_name, true/false (enable,disable), time)
+    def set_sensor(self, sensor, status, interval):
+        if status:
+            status = 0x01
+        else:
+            status = 0x00
+        if self.dict[sensor] == 0x01 or self.dict[sensor] == 0x02 or self.dict[sensor] == 0x03:
+            interval = interval * 1000
+        self.write([0x05, self.dict[sensor], status] + list(interval.to_bytes(4, 'big')))
+        res = self.read()
+        success = True
+        for i in range(len(res)):
+            if (i == 0):
+                if res[i] != '12':
+                    success = False
+  
+            elif i == 14:
+                if res[i] != '0a':
+                    success = False
+            elif res[i] != '00':
+                print("Error Code: " + str(i) + " , Flag " + res[i])
+                success = False
+            
+        if success:
+            if status:
+                print("Sensor: " + sensor + " successfully enabled!")
+            else:
+                print("Sensor: " + sensor + " successfully disabled!")
+        else:
+            print("Error setting sensor: " + sensor)
+        return success
+
+        
+        
+    # 3.6 SET Erase
+    # Requests Kallisto to erase file from its default storage method
+
+    # TO DO
+    def set_erase(self, path_length, path):
+        self.write([0x06, path_length, path])
+        res = self.read()
+        print("ok")
+        print(res)
+        return False
+
+    
     # 3.7
     def set_rtc(self, month, day, year, weekday, hour, minutes, seconds, centiseconds):
         self.write([0x07] + convert_to_hex(month, day, year, weekday, hour, minutes, seconds, centiseconds))
@@ -89,6 +158,69 @@ class Kallisto:
         res = self.read()
         return res
 
+    # 3.9 GET Battery
+    # Requests Kallisto to provide the current Battery state of charge value
+    def get_battery(self):
+        self.write([0x09, 0x01])
+        res = self.read()
+        print(res)
+        if (res[0] != '1e' or  res[3] != '0a'):
+            print("Error getting battery")
+            return False
+        soc = res[1]
+        status = res[2]
+        print("Battery: " + str(soc) + "%")
+        print("Charging..." if status == '01' else "Not charging...")
+        
+        return True
+    
+    # 3.10 GET Storage List
+    # Requests Kallisto to provide list of files in the root directory of the default storage method.
+
+    # TO DO
+    def get_storage_list(self):
+        self.write([0x0a, 0x01])
+        res = self.read()
+        print(res)
+        return False
+    
+    # 3.11 GET Status
+    # Requests Kallisto to provide the current status of each sensor. 
+    # Including: if the sensor is enabled, if the sensor data is being stored, if the sensor data is being streamed and the current sampling period.
+
+    # TO DO
+    def get_status(self):
+        self.write([0x0b, 0x01])
+        res = self.read()
+        print(res)
+        return False
+
+    # 3.12 SET Calibration
+
+    def set_calibration(self, sensor):
+        self.write([0x0c, self.dict[sensor]])
+        res = self.read()
+        success = True
+        for i in range(len(res)):
+            if i == 0:
+                if res[i] != '27':
+                    success = False
+            elif i == 14:
+                if res[i] != '0a':
+                    success = False   
+            elif res[i] != '00':
+                print("Error calibrating sensor: " + sensor)
+                success = False
+                
+        if success:
+            print("Sensor: " + sensor + " successfully calibrated!")
+            return True
+        else:
+            return False
+        
+
+      
+
 
 if __name__ == '__main__':
 
@@ -100,11 +232,19 @@ if __name__ == '__main__':
             possible_ports[port] = hwid
 
     sensor = Kallisto('COM3', possible_ports['COM3'])
-    sensor.set_sensor('accel', True, 100)
-    print(sensor.read())
+    #sensor.set_sensor('accel', False, 100)
+    sensor.set_calibration('accel')
+    
+    """
     timeleft = time.time() + 3
+  
     while True:
         print(sensor.read())
         if timeleft == 0:
             break
         timeleft = timeleft - time.time()
+        """
+
+
+        
+        
